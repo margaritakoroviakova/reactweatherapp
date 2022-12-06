3e5b7b59e42087836327cce12a38bac60e413757
margaritakoroviakova <koroviakovam@gmail.com>
2022-12-06 22:12:22 +0200

    Finally built search engine but without icons, unit conversion and ...

---
+++ src/Weather.js
@@ -0,0 +1,67 @@
+import './App.css';
+import axios from "axios";
+import { useState } from 'react';
+import WeatherInfo from "./WeatherInfo";
+
+export default function Weather(props) {
+let [weather, setWeather] = useState({ready:false});
+let [city, setCity] = useState(props.defaultCity);
+
+
+function weatherResponse (response) {
+  setWeather({
+  ready: true,
+  city: response.data.city,
+  temperature: response.data.temperature.current,
+  humidity: response.data.temperature.humidity,
+  wind: response.data.wind.speed,
+  description: response.data.condition.description,
+  });
+} 
+
+function showCity (event) {
+  setCity(event.target.value);
+}
+
+function handleSubmit (event) {
+  event.preventDefault();
+  searching();
+}
+
+function searching () {
+  let apiKey = "0f037oa9b63a1bde26ff4t7c8b350ada";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(weatherResponse);
+} 
+
+if (weather.ready) {
+  return (
+    <div className="App">
+          <form className="form" id="searching-form" onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-7 input">
+                <input
+                  type="text"
+                  className="formControl"
+                  id="city"
+                  placeholder="Enter a city"
+                  autoComplete="off"
+                  onChange={showCity}
+                />
+              </div>
+              <div className="col-4">
+                <input type="submit" className="btn btn-primary primary" value='Search'/>
+              </div>
+            </div>
+          </form>
+          <WeatherInfo data={weather}/>
+    </div>
+  );
+} else {
+  searching();
+  return "Wait a second, please..."
+}
+}
+
+
+
---
+++ src/WeatherInfo.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+export default function WeatherInfo (props) {
+    return (
+    <div className="WeatherInfo">
+        <h3>{props.data.city}</h3>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                <div className="temperature">{Math.round(props.data.temperature)} °C</div>
+                <div className="iconic">Icon</div>
+                </div>
+                <div className="col-6">
+                    <div className="parameters">
+                        <div>Humdity: {props.data.humidity}%</div>
+                         <div>Wind: {Math.round(props.data.wind)} km/h</div>
+                          <div>Description: {props.data.description}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
