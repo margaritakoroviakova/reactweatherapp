637ac43f4c6232a731b1477dd0154304f585c4b5
margaritakoroviakova <koroviakovam@gmail.com>
2022-12-23 16:14:05 +0200

    Forecast is here

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,7 +15,15 @@ export default function WeatherForecast (props) {
     return (
     <div className="WeatherForecast">
         <div className="row">
-                <WeatherForecastDay forecast={forecast[0]}/>
+                {forecast.map(function(dailyForecast, index) {
+                    if (index < 5) {
+                        return (
+                            <div className="col" key={index}><WeatherForecastDay data={dailyForecast}/> </div>
+                        ); 
+                    } else {
+                        return null;
+                    }
+                })}
             </div>
         </div>
     );
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,26 +2,26 @@ import React from "react";
 
 export default function WeatherForecastDay (props) {
 function maxTemp() {
-    let temperature = Math.round(props.forecast.temperature.maximum);
+    let temperature = Math.round(props.data.temperature.maximum);
     return `${temperature}`;
 }
 
 function minTemp() {
-    let temperature = Math.round(props.forecast.temperature.minimum);
+    let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}`;
 }
 
 function day() {
-    let date = new Date(props.forecast.time*1000);
+    let date = new Date(props.data.time*1000);
     let day = date.getDay();
-    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturdays"]
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     return days[day];
 }
 
 return (
 <div className="forecastDay">
 <div>{day()}</div>
-<div><img src={props.forecast.condition.icon_url} alt=""></img></div>
+<div><img src={props.data.condition.icon_url} alt=""></img></div>
 <div className="forecastTemperature">
 <span className="tempMax">{maxTemp()}°</span>
 <span className="tempMin ms-1">{minTemp()}°</span>
